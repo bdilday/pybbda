0acaa32479e20559294765ec313c328f39a323a4
Ben Dilday <ben.dilday.phd@gmail.com>
2020-01-26 11:54:33 -0500

    adds setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name="pybaseballdatana",
+    version="1.0",
+    description="Baseball data and analysis in Python",
+    author="Ben Dilday",
+    author_email="ben.dilday.phd@gmail.com",
+    packages=["pybaseballdatana"],
+)
