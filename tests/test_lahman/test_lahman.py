734156bd6cac27b2f2477bea61a978138213e13d
Ben Dilday <ben.dilday.phd@gmail.com>
2020-02-13 07:54:12 -0500

    updates tests

--- tests/test_lahman/test_lahman.py
+++ tests/test_lahman/test_lahman.py
@@ -3,4 +3,5 @@ from pybaseballdatana.lahman.data import LahmanData
 
 def test_lahman_datadum():
     lahman_data = LahmanData()
-    assert lahman_data.batting == 1
+    assert len(lahman_data.batting) == 105861
+    
