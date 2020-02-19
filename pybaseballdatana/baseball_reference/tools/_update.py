fd0adc68c3c8ecfb038596b299fb3ed9a6fca2fc
Ben Dilday <ben.dilday.phd@gmail.com>
2020-02-19 09:26:57 +0100

    baseball reference download tools

--- pybaseballdatana/baseball_reference/tools/_update.py
+++ pybaseballdatana/baseball_reference/tools/_update.py
@@ -11,27 +11,35 @@ import shutil
 import logging
 import csv
 
+
 WAR_BATTING_URL = "https://www.baseball-reference.com/data/war_daily_bat.txt"
 WAR_PITCHING_URL = "https://www.baseball-reference.com/data/war_daily_pitch.txt"
 
 
 def _download_csv(url):
     logging.info("downloading file from {}".format(url))
-    
-    archive = zipfile.ZipFile(BytesIO(requests.get(LAHMAN_URL, stream=True).content))
-    target = tempfile.gettempdir()
-    archive.extractall(path=target)
-    return target
+    response = requests.get(url, stream=True)
+    if response.status_code != 200:
+        logging.info("there was a download error code={}", response.status_code)
+        raise FileNotFoundError
+    it = response.iter_lines()
+    return list(it)
 
 
-def _extract(target):
-    output_path = pathlib.Path(__file__).parent / "BaseballReference"
-    extracted_files = glob.glob(os.path.join(target, "**", "*csv"), recursive=True)
-    for extracted_file in extracted_files:
-        shutil.copy(extracted_file, output_path)
+def _extract(lines, file_name):
+    output_path = (
+        pathlib.Path(__file__).absolute().parent.parent.parent
+        / "assets"
+        / "BaseballReference"
+        / file_name
+    )
+    logging.info("saving file to {}".format(output_path))
+    with open(output_path, "w") as fh:
+        csvw = csv.writer(fh)
+        csvw.writerows(lines)
 
 
 def _update():
     for url in [WAR_BATTING_URL, WAR_PITCHING_URL]:
-    target = _download()
-    _extract(target)
+        lines = _download_csv(url)
+        _extract(lines, os.path.basename(url))
