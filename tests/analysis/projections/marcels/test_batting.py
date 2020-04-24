762de658c4fba00e3246e75e120777ccb0b019ac
Ben Dilday <ben.dilday.phd@gmail.com>
2020-04-24 15:59:41 -0400

    updates tests

--- tests/analysis/projections/marcels/test_batting.py
+++ tests/analysis/projections/marcels/test_batting.py
@@ -3,13 +3,24 @@ from pybaseballdatana.analysis.projections.marcels import MarcelProjectionsBatti
 from pandas import DataFrame
 import pytest
 
+
+
+def test_batting_projections():
+    md = MarcelProjectionsBatting()
+    md.projections(2020)
+
 @pytest.mark.parametrize("season, expected",
-                         [(2004, 42)])
-def test_batting_projections(season, expected):
+                         [
+                             (2020, 36),
+                             (2019, 38),
+                             (2018, 41),
+                             (2017, 34),
+                             (2004, 42)])
+def test_batting_metric_projections(season, expected):
 
     md = MarcelProjectionsBatting()
-    proj = md.projections(season)
-    assert int(proj.HR.max()) == expected
+    proj = md.metric_projection("HR", season)
+    assert round(proj.HR.max()) == expected
 
 def test_batting_bad_data():
     stats_df = DataFrame({"x": [1,2,3]})
