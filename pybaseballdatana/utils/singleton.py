4e16975252c6b5d4de5ea055bec3449a73c1b380
Ben Dilday <ben.dilday.phd@gmail.com>
2020-04-25 14:05:11 -0400

    makes LahmanData a singleton

---
+++ pybaseballdatana/utils/singleton.py
@@ -0,0 +1,13 @@
+
+# https://www.python.org/download/releases/2.2.3/descrintro/#__new__
+
+class Singleton(object):
+    def __new__(cls, *args, **kwargs):
+        it = cls.__dict__.get("__it__")
+        if it is not None:
+            return it
+        cls.__it__ = it = object.__new__(cls)
+        it.init(*args, **kwargs)
+        return it
+    def init(self, *args, **kwargs):
+        pass
--- tests/test_lahman/test_lahman.py
+++ tests/test_lahman/test_lahman.py
@@ -14,3 +14,9 @@ def test_lahman_datadum(lahman_data):
 def test_missing_path(lahman_data):
     with pytest.raises(AttributeError):
         lahman_data.battingX
+
+def test_lahman_singelton():
+    ld1 = LahmanData()
+    ld2 = LahmanData()
+    assert ld1 is ld2
+
