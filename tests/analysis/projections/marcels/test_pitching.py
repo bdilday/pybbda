6db934b481fbc25d60ca08ab774e9cce5dba2a7e
Ben Dilday <ben.dilday.phd@gmail.com>
2020-04-24 21:04:37 -0400

    updates pitching tests

--- tests/analysis/projections/marcels/test_pitching.py
+++ tests/analysis/projections/marcels/test_pitching.py
@@ -4,10 +4,23 @@ from pandas import DataFrame
 import pytest
 
 def test_pitching_projections():
+    md = MarcelProjectionsPitching()
+    md.projections(2020)
+
+@pytest.mark.parametrize("season, expected",
+                         [
+                             (2020, 242),
+                             (2019, 235),
+                             (2018, 229),
+                             (2017, 224),
+                             (2004, 207)])
+def test_pitching_metric_projections(season, expected):
 
     md = MarcelProjectionsPitching()
-    proj = md.projections(2004)
-    assert int(proj.SO.max()) == 207
+    proj = md.metric_projection("SO", season)
+    assert round(proj.SO.max()) == expected
+
+
 
 def test_pitching_bad_data():
     stats_df = DataFrame({"x": [1,2,3]})
