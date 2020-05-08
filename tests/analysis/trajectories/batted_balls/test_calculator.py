2268200d82e184e9672ee5846ffb3f04d5e57cca
Ben Dilday <ben.dilday.phd@gmail.com>
2020-05-07 20:52:05 -0400

    update tests

--- tests/analysis/trajectories/batted_balls/test_calculator.py
+++ tests/analysis/trajectories/batted_balls/test_calculator.py
@@ -2,6 +2,7 @@ import numpy as np
 from pybaseballdatana.analysis.trajectories.batted_balls.calculator import (
     BattedBallTrajectory,
 )
+from pybaseballdatana.analysis.trajectories.batted_balls.utils import sin_in_degrees
 import pytest
 
 
@@ -24,6 +25,7 @@ def test_batted_ball_trajectory():
 
 def test_projectile_motion():
     batted_ball_traj = BattedBallTrajectory(magnus_strength=0, drag_strength=0)
+
     launch_angle = 90
     launch_dir = 0
     initial_speed = 100
@@ -42,6 +44,33 @@ def test_projectile_motion():
     assert traj.z.max() == pytest.approx(
         0.5
         * (initial_speed * batted_ball_traj.env_parameters.unit_conversions.mph_to_fts)
+        ** 2
+        / batted_ball_traj.env_parameters.g_gravity,
+        1,
+    )
+
+    launch_angle = 30
+    launch_dir = 0
+    initial_speed = 100
+    initial_spin = 1
+    spin_angle = 0
+    traj = batted_ball_traj.get_trajectory(
+        initial_speed, launch_angle, launch_dir, initial_spin, spin_angle
+    )
+    assert traj.t.iloc[-1] == pytest.approx(
+        2
+        * initial_speed
+        * batted_ball_traj.env_parameters.unit_conversions.mph_to_fts
+        / batted_ball_traj.env_parameters.g_gravity,
+        0.5,
+    )
+    assert traj.z.max() == pytest.approx(
+        0.5
+        * (
+            initial_speed
+            * sin_in_degrees(launch_angle)
+            * batted_ball_traj.env_parameters.unit_conversions.mph_to_fts
+        )
         ** 2
         / batted_ball_traj.env_parameters.g_gravity,
         1,
