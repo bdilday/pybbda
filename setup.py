0c661472e878a63cf443389e61ad130778341d9f
Ben Dilday <bdilday@users.noreply.github.com>
2020-06-07 22:04:10 -0400

    adds version (#27)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pybaseballdatana",
-    version="1.0",
+    version="0.1.0",
     description="Baseball data and analysis in Python",
     author="Ben Dilday",
     author_email="ben.dilday.phd@gmail.com",
