728ea87b453c0f183e041f7f66cdb1d5feaccabc
Ben Dilday <bdilday@users.noreply.github.com>
2020-06-11 21:06:55 -0400

    retrosheet data (#28)
    * updates chadwick requirement
    
    * updates retrosheet tools
    
    * adds sqlalchemy
    
    * updates pychadwick version
    
    * liniting

---
+++ pybaseballdatana/data/sources/retrosheet/_update.py
@@ -0,0 +1,62 @@
+from io import BytesIO
+import os
+import requests
+import zipfile
+import glob
+import pathlib
+import re
+
+import logging
+from pybaseballdatana.data.sources.retrosheet.data import RetrosheetData
+
+logger = logging.getLogger(__name__)
+
+RETROSHEET_URL = "https://github.com/chadwickbureau/retrosheet/archive/master.zip"
+
+
+def _download(output_root):
+    target = os.path.join(output_root, "retrosheet")
+    archive_path = os.path.join(target, "retrosheet-master")
+    if os.path.exists(archive_path):
+        logger.info("path %s exists, not downloading", archive_path)
+    else:
+        logger.info("downloading file from %s", RETROSHEET_URL)
+        archive = zipfile.ZipFile(
+            BytesIO(requests.get(RETROSHEET_URL, stream=True).content)
+        )
+        archive.extractall(path=target)
+    return archive_path
+
+
+def _validate_path(output_root):
+    output_root = output_root or pathlib.Path(__file__).parent.parent / "assets"
+    if not os.path.exists(output_root):
+        raise ValueError(f"Path {output_root} does not exist")
+    if not os.path.isdir(output_root):
+        raise ValueError(f"Path {output_root} must be a directory")
+
+
+def _filter_event_files(event_files, min_year, max_year):
+    result = []
+    for event_file in event_files:
+        year = int(re.search("^([0-9]{4})", os.path.basename(event_file)).group(1))
+        if min_year <= year <= max_year:
+            logger.info("including file %s", event_file)
+            result.append(event_file)
+
+    return result
+
+
+def _update(output_root=None, min_year=1871, max_year=2019, overwrite=False):
+    output_root = output_root or pathlib.Path(__file__).parent.parent / "assets"
+    _validate_path(output_root)
+    retrosheet_data = RetrosheetData(output_root)
+    target = _download(output_root)
+    event_files = _filter_event_files(
+        glob.glob(os.path.join(target, "event", "regular", "*EV*")), min_year, max_year
+    )
+
+    retrosheet_data.create_database()
+    retrosheet_data.initialize_table(retrosheet_data.df_from_file(event_files[0]))
+    for event_file in event_files[1:]:
+        retrosheet_data.update_table(retrosheet_data.df_from_file(event_file))
--- pybaseballdatana/data/sources/retrosheet/data.py
+++ pybaseballdatana/data/sources/retrosheet/data.py
@@ -3,25 +3,53 @@ import psycopg2
 import pandas as pd
 import logging
 
+from sqlalchemy import create_engine
+from pychadwick.chadwick import Chadwick
+from pybaseballdatana import PYBBDA_DATA_ROOT
+
 logger = logging.getLogger(__name__)
 
 
 class RetrosheetData:
-    def __init__(self, database="retrosheet"):
-        self.conn = psycopg2.connect(
+    def __init__(self, data_root=None):
+        self.data_root = data_root or PYBBDA_DATA_ROOT
+        self.db_dir = os.path.join(self.data_root, "retrosheet")
+        self.db_path = os.path.join(self.db_dir, "retrosheet.db")
+        self._engine = None
+        self.chadwick = Chadwick()
+
+    def _connect_to_postgres(self, database="retrosheet"):
+        conn = psycopg2.connect(
             database=database,
             user=os.environ["PSQL_USER"],
             password=os.environ["PSQL_PASS"],
             port=os.environ["PSQL_PORT"],
         )
-        self._event = None
+        return conn
+
+    def create_database(self):
+        if not os.path.exists(self.db_dir):
+            os.makedirs(self.db_dir, exist_ok=True)
+        self._engine = create_engine(f"sqlite:///{self.db_path}", echo=False)
+
+    @property
+    def engine(self):
+        if not self._engine:
+            self._engine = create_engine(f"sqlite:///{self.db_path}", echo=False)
+        return self._engine
+
+    def initialize_table(self, df, conn=None):
+        conn = conn or self.engine
+        df.to_sql("event", conn, index=False, if_exists="replace")
+
+    def update_table(self, df, conn=None):
+        logger.info("updating table with %s", df.GAME_ID.iloc[0])
+        conn = conn or self.engine
+        df.to_sql("event", conn, index=False, if_exists="append")
 
     def query(self, query):
-        return pd.read_sql_query(query, self.conn)
+        return pd.read_sql_query(query, self.engine)
 
-    # @property
-    # def event(self):
-    #     if self._event is None:
-    #         logging.info("loading event data from sql")
-    #         self._event = pd.read_sql_query("select * from event", self.conn)
-    #     return self._event
+    def df_from_file(self, file_path):
+        games = self.chadwick.games(file_path)
+        return self.chadwick.games_to_dataframe(games)
