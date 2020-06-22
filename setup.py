ffb6bd2d6838a2a699136e573c7124254e0bb06b
Ben Dilday <bdilday@users.noreply.github.com>
2020-06-21 20:02:20 -0400

    bump version to v0.1.3 (#33)

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("requirements.txt", "r") as fh:
 
 setup(
     name="pybbda",
-    version="0.1.2",
+    version="0.1.3",
     author="Ben Dilday",
     author_email="ben.dilday.phd@gmail.com",
     description="Baseball data and analysis in Python",
