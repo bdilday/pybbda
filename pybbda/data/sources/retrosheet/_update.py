82f6ef03085e6c4d2a6b8363ec280ec71516795f
Ben Dilday <ben.dilday.phd@gmail.com>
2020-06-25 18:19:10 -0400

    adds retrosheet people crosswalk table

--- pybbda/data/sources/retrosheet/_update.py
+++ pybbda/data/sources/retrosheet/_update.py
@@ -3,6 +3,7 @@ import os
 import requests
 import zipfile
 import glob
+import gzip
 import pathlib
 import re
 
@@ -13,6 +14,48 @@ from pybbda.data.sources.retrosheet.data import RetrosheetData
 logger = logging.getLogger(__name__)
 
 RETROSHEET_URL = "https://github.com/chadwickbureau/retrosheet/archive/master.zip"
+
+
+# TODO dry it up
+def _download_csv(url):
+    logger.info("downloading file from {}".format(url))
+    response = requests.get(url, stream=True)
+    if response.status_code != 200:
+        logger.info("there was a download error code={}", response.status_code)
+        raise FileNotFoundError
+    it = response.iter_lines()
+    return list(it)
+
+
+def _save(lines, file_name, output_path):
+    output_file_path = os.path.join(output_path, file_name)
+    output_payload = "\n".join(map(lambda s: str(s, "utf-8"), lines))
+    print(len(output_payload))
+    logger.info("saving file to {}".format(output_file_path))
+    with gzip.open(output_file_path, "wb") as fh:
+        fh.write(bytes(output_payload, encoding="utf-8"))
+
+
+def _update_file(
+    url, output_root, output_filename=None, rows_filter=None, overwrite=False
+):
+    output_filename = output_filename or ".".join((os.path.basename(url), "gz"))
+    output_path = os.path.join(output_root, "retrosheet")
+    os.makedirs(output_path, exist_ok=True)
+
+    output_file_path = os.path.join(output_path, output_filename)
+    if os.path.exists(output_file_path):
+        if not overwrite:
+            logger.info("file %s exists, not overwriting", output_file_path)
+            return
+        else:
+            logger.warning("file %s exists, but overwriting", output_file_path)
+
+    lines = _download_csv(url)
+    if rows_filter is not None:
+        lines = rows_filter(lines)
+
+    _save(lines, output_filename, output_path)
 
 
 def _download(output_root):
@@ -61,6 +104,10 @@ def _update(output_root=None, min_year=1871, max_year=2019, create_database=Fals
     output_root = output_root or pathlib.Path(__file__).parent.parent / "assets"
     _validate_path(output_root)
     retrosheet_data = RetrosheetData(output_root)
+
+    for table_url in retrosheet_data._SOURCE_URLS.values():
+        _update_file(table_url, output_root)
+
     target = _download(output_root)
     event_files = _filter_event_files(
         glob.glob(os.path.join(target, "event", "regular", "*EV*")), min_year, max_year
