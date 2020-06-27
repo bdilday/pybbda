eb02c6dfbb848b428d463c6772c9a62f0860b1ce
Ben Dilday <ben.dilday.phd@gmail.com>
2020-06-26 22:32:09 -0400

    bumps version to v0.1.7

--- pybbda/__init__.py
+++ pybbda/__init__.py
@@ -5,7 +5,7 @@ import logging
 logging.basicConfig(format="%(levelname)s:%(name)s:%(module)s:%(message)s")
 logger = logging.getLogger("pybbda")
 
-_version = "0.1.6"
+_version = "0.1.7"
 
 PYBBDA_LOG_LEVEL_NAME = os.environ.get("PYBBDA_LOG_LEVEL", "")
 _PYBBDA_LOG_LEVEL_MAP = {
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("requirements.txt", "r") as fh:
 
 setup(
     name="pybbda",
-    version="0.1.6",
+    version="0.1.7",
     author="Ben Dilday",
     author_email="ben.dilday.phd@gmail.com",
     description="Baseball data and analysis in Python",
