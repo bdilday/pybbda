5d23b374cd5fad30df2a69c548c00199268fd1e6
Ben Dilday <ben.dilday.phd@gmail.com>
2020-09-04 13:22:48 -0400

    updates statcast tests

--- tests/data/test_baseball_reference/test_baseball_reference_data.py
+++ tests/data/test_baseball_reference/test_baseball_reference_data.py
@@ -10,13 +10,13 @@ def baseball_ref_data():
 def test_war_bat(baseball_ref_data):
     war_bat = baseball_ref_data.war_bat
     assert war_bat.year_ID.min() == 1871
-    assert war_bat.year_ID.max() == 2019
+    assert war_bat.year_ID.max() == 2020
 
 
 def test_war_pitch(baseball_ref_data):
     war_pitch = baseball_ref_data.war_pitch
     assert war_pitch.year_ID.min() == 1871
-    assert war_pitch.year_ID.max() == 2019
+    assert war_pitch.year_ID.max() == 2020
 
 
 def test_missing_path(baseball_ref_data):
