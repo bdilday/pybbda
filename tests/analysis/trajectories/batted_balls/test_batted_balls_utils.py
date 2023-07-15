8fccc27f6e163635ba9bb35e3c661cea8e0b3bfb
Ben <ben.dilday.phd@gmail.com>
2023-07-15 14:26:03 -0400

    approx test for trig

--- tests/analysis/trajectories/batted_balls/test_batted_balls_utils.py
+++ tests/analysis/trajectories/batted_balls/test_batted_balls_utils.py
@@ -5,7 +5,7 @@ import pytest
 
 def test_trig_utils():
     deg = 1
-    assert cos_in_degrees(deg) ** 2 + sin_in_degrees(deg) ** 2 == 1
+    assert cos_in_degrees(deg) ** 2 + sin_in_degrees(deg) ** 2 == pytest.approx(1)
 
     deg = 30
     assert cos_in_degrees(deg) == pytest.approx(sqrt(3) / 2)
